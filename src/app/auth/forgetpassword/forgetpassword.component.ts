4dc1feb58276a00808c804f2e45fc212b3d4fa67
MalekMohamed <mr.magic974@gmail.com>
2019-12-23 12:20:07 +0200

    Add forget password

---
+++ src/app/auth/forgetpassword/forgetpassword.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from '../../shared/services/firebase/auth.service';
+
+type UserFields = 'email';
+type FormErrors = { [u in UserFields]: string };
+
+@Component({
+  selector: 'app-forgetpassword',
+  templateUrl: './forgetpassword.component.html',
+  styleUrls: ['./forgetpassword.component.scss']
+})
+export class ForgetpasswordComponent implements OnInit {
+
+  public newUser = false;
+  public user: firebase.User;
+  public forgetpasswordForm: FormGroup;
+  public formErrors: FormErrors = {
+    'email': ''
+  };
+  public errorMessage: any;
+
+  constructor(public authService: AuthService,
+    private afauth: AngularFireAuth, private fb: FormBuilder,
+    private router: Router) {
+    this.forgetpasswordForm = fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+
+  // Simple forgetpassword
+  ResetPassword(form) {
+    this.authService.showLoader = true;
+    this.authService.ForgotPassword(this.forgetpasswordForm.value['email']);
+  }
+
+}
