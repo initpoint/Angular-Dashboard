9218bc15323e49f67c26a364261c08ef3aa44c55
MalekMohamed <mr.magic974@gmail.com>
2020-01-14 00:50:00 +0200

    update items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ItemsService} from 'src/app/shared/services/firebase/items.service';
-import CustomeStore from 'devextreme/data/custom_store';
-import DataSource from 'devextreme/data/data_source';
+
 @Component({
     selector: 'app-items',
     templateUrl: './items.component.html',
@@ -10,28 +9,23 @@ import DataSource from 'devextreme/data/data_source';
 export class ItemsComponent implements OnInit {
     source: any;
     lang;
-    materialSelectedRows= {};
-    rankingSelectedRows= {};
-    applyFilterTypes = {
-        key: "auto",
-        name: "Immediately"
-    };
+    materialSelectedRows = {};
+    rankingSelectedRows = {};
+
     constructor(private itemsService: ItemsService) {
-        this.source = new DataSource(new CustomeStore({
-            key: 'code',
-            load: (opts) => this.itemsService.getItems(opts),
-            update: (key, newValues) => this.itemsService.updateItem(key, newValues),
-            remove: (key) => this.itemsService.updateItem(key, {isActive: false})
-        }));
+        this.itemsService.getItemsSync().subscribe(res => {
+            this.source = Object.keys(res.data()).map(key => res.data()[key]);
+        });
+
     }
 
     ngOnInit() {
         this.lang = localStorage.getItem('lang') === 'ar';
     }
 
-    materialRowSelected(event, key,items,collapsedItems,component) {
+    materialRowSelected(event, key, items, collapsedItems, component) {
         this.materialSelectedRows[key[1]] = event.value;
-        component.expandRow(key)
+        component.expandRow(key);
         if (items) {
             items.forEach(item => {
                 if (event.value) {
@@ -51,23 +45,26 @@ export class ItemsComponent implements OnInit {
             });
         }
     }
+
     rankingRowSelected(event, ranking) {
         this.rankingSelectedRows[ranking.key[0]] = event.value;
-        ranking.component.expandRow(ranking.key)
+        ranking.component.expandRow(ranking.key);
         if (ranking.data.items) {
             ranking.data.items.forEach(item => {
-               this.materialRowSelected(event,[ranking.key[0],item.key],item.items,item.collapsedItems,ranking.component)
+                this.materialRowSelected(event, [ranking.key[0], item.key], item.items, item.collapsedItems, ranking.component);
             });
         }
         if (ranking.data.collapsedItems) {
             ranking.data.collapsedItems.forEach(item => {
-            this.materialRowSelected(event,[ranking.key[0],item.key],item.items,item.collapsedItems,ranking.component)
-             });
+                this.materialRowSelected(event, [ranking.key[0], item.key], item.items, item.collapsedItems, ranking.component);
+            });
         }
     }
+
     getMaterialSelectValue(material) {
         return this.materialSelectedRows[material.key[1]];
     }
+
     getRankingSelectValue(ranking) {
         return this.rankingSelectedRows[ranking.key[0]];
     }
