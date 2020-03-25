fd67977357be1bbbadc075de934090e059b873c4
MalekMohamed <mr.magic974@gmail.com>
2020-03-25 19:24:13 +0200

    add delete selected row in logs

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit, Output} from '@angular/core';
+import {Component, OnInit, Output, ViewChild} from '@angular/core';
 import {LogsService} from '../../shared/services/firebase/logs.service';
 import CustomeStore from 'devextreme/data/custom_store';
 import DataSource from 'devextreme/data/data_source';
+import {DxDataGridComponent} from 'devextreme-angular';
 
 @Component({
     selector: 'app-logs',
@@ -9,10 +10,12 @@ import DataSource from 'devextreme/data/data_source';
     styleUrls: ['./logs.component.scss']
 })
 export class LogsComponent implements OnInit {
+    @ViewChild('logsGrid', {static: false}) grid: DxDataGridComponent;
     logSource: DataSource;
     logData: CustomeStore;
     lang = localStorage.getItem('lang') == 'ar';
     fieldView: boolean = true;
+    selectedRows: number[];
 
     constructor(private logsService: LogsService) {
         this.logData = new CustomeStore({
@@ -34,6 +37,17 @@ export class LogsComponent implements OnInit {
         });
     }
 
+    removeAllSelection() {
+        if (confirm('Are you sure about this?\n This will delete all the data you have\n This cannot be undone ' +
+            '\nهل تريد مسح كل البيانات؟ \n لا يمكن التراجع عن ذلك')) {
+            this.selectedRows.forEach(key => {
+                this.logsService.deleteLog(key).then(res=> {
+                    this.logData.remove(key);
+                });
+            });
+            this.grid.instance.refresh();
+        }
+    }
 
     ngOnInit() {
     }
